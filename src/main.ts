78266d3fdf324d854dcc718274dc56ab0c4e8d22
R3pic <101osc@naver.com>
2025-01-24 22:41:35 +0900

    feat: swagger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const logger = new Logger('Bootstrap');
+
+    const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('꾸러미탑 API ')
+        .setDescription('꾸러미탑 API')
+        .setVersion('0.1')
+        .build();
+
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
+
+    await app.listen(process.env.PORT ?? 3000);
+    logger.log('Application started');
 }
+
 bootstrap();
